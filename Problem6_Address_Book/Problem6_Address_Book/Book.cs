2d8d74db975608bf01f83c389992e3c36667c89a
Kajal <kajalpatil158@gmail.com>
2021-03-14 12:52:25 +0530

    [Kajal Patil]Added: Adrress Book Record

---
+++ Problem6_Address_Book/Problem6_Address_Book/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem6_Address_Book
+{
+    class Book
+    {
+        
+    }
+}
---
+++ Problem6_Address_Book/Problem6_Address_Book/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem6_Address_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel Come To Address Book System:");
+            Console.WriteLine("Enter First Name:");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name:");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address:");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City:");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter state:");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter Zip:");
+            string Zip = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number:");
+            long PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email:");
+            string Email = Console.ReadLine();
+
+
+
+        }
+    }
+}
